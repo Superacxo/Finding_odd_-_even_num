380ff4719ca7a2d93e43bb31b63956e664108013
Python <168913086+Superacxo@users.noreply.github.com>
2024-05-15 11:10:24 +0530

    Create Find_odd_even_num.py

---
+++ Find_odd_even_num.py
@@ -0,0 +1,15 @@
+# odd num...
+num_ = int(input(":")) 
+def odd(num):
+  for i in range (num):
+    print(i*2+1)
+odd(num_)
+
+# even num... 
+def even(n):
+  ng = n
+  for i in range(ng):
+    ig = i*2
+    print(ig)
+even(num_)
+
